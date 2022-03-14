4c107eccf93abfb498e5877b7a7a1130fc9325ec
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2022-03-14 14:56:00 +0900

    Fix dependency of revisions for alembic

--- jupyterhub/alembic/versions/1c1c8e328766_mail_address.py
+++ jupyterhub/alembic/versions/1c1c8e328766_mail_address.py
@@ -7,7 +7,7 @@ Create Date: 2020-05-02 16:37:07.856516
 """
 # revision identifiers, used by Alembic.
 revision = '1c1c8e328766'
-down_revision = '4dc2d5a8c53c'
+down_revision = '833da8570507'
 branch_labels = None
 depends_on = None
 
