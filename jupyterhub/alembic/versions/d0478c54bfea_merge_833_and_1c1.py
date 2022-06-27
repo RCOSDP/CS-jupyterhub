187456da000b4a745b179052fa1ffbb35774c58d
kimuraM333 <kimura@lifematics.co.jp>
2022-06-27 10:43:08 +0900

    merge alembic revision

---
+++ jupyterhub/alembic/versions/d0478c54bfea_merge_833_and_1c1.py
@@ -0,0 +1,24 @@
+"""merge 833 and 1c1
+
+Revision ID: d0478c54bfea
+Revises: 833da8570507, 1c1c8e328766
+Create Date: 2022-06-27 10:42:07.716403
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd0478c54bfea'
+down_revision = ('833da8570507', '1c1c8e328766')
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
